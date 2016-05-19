923713b4a4c28169521290417810abe079b85816
Stephanie Tong <tong.stephanie_mylinh@yahoo.com>
2016-05-18 20:56:18 -0700

    Added temporary login page
    Notes:
    - Login isn't implemented yet.
    - We haven't done user authentication.

--- StationerryWeb/StationerryWebApp/urls.py
+++ StationerryWeb/StationerryWebApp/urls.py
@@ -36,5 +36,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'^$', views.login, name='home'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^main/$', views.main, name='main'),
 ]
--- StationerryWeb/StationerryWebApp/views.py
+++ StationerryWeb/StationerryWebApp/views.py
@@ -10,5 +10,13 @@ from django.http import HttpResponse
 def foo(request):
     return HttpResponse("Hello World!")
 
+# This is the homepage
 def home(request):
+	return render(request, 'stationerry/login.html', {})
+
+def login(request):
+	return render(request, 'stationerry/login.html', {})
+
+# This is the main page after the user logs in
+def main(request):
 	return render(request, 'stationerry/base.html', {})
