3a44c967fa4469ef36531a1b69b9fe97187eb9a6
kawaii5 <oc3anazns3a@gmail.com>
2016-05-26 17:51:15 -0700

    added users to models.py

--- StationerryWeb/Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn StationerryWeb.wsgi:application --log-file - 
--- StationerryWeb/StationerryBackend/models.py
+++ StationerryWeb/StationerryBackend/models.py
@@ -5,15 +5,25 @@ from django.db import models
 # Django will give each model an unique ID as a primary key
 # when we do not specify one. (^_^)
 
+class Users(models.Model) :
+    User_Name = models.TextField(primary_key=True)
+    Password = models.TextField()
+    Email = models.TextField()
+    
+    def __unicode__(self) :
+        return self.App_Name + " " + self.App_Version
+
+# Defines for Django - makes schema for app - set the database
 class App(models.Model) :
     App_Name = models.TextField()
     App_Version = models.TextField()
     Platform = models.TextField()
+    App_Name = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='Name')
 
     def __unicode__(self) :
         return self.App_Name + " " + self.App_Version
-    pass
 
+# schema for bugreport
 class BugReport(models.Model) :
     Error_Type = models.TextField()
     Error_Message = models.TextField()
